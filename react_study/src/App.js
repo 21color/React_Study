6a39ac58a65d8d8aea8af77605dff0503db3b779
hanalee <lehhha0110@gmail.com>
2022-07-07 22:30:03 +0900

    11.component

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,16 +1,27 @@
 
 import React from 'react';
 import {Component} from 'react';
+import './style.css';
+
+class Subject extends Component {
+  render () {
+    return (
+      <header>
+        <h1>WEB1</h1>
+        World Wide Web!
+      </header> // 주의점! 항상 닫히는..최상위 태그가 존재해야함
+    );
+    }
+} //요부분을 잘 봐라! 요게 컴포넌트! 
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <h1>hello World! </h1>
+        <Subject></Subject>
       </div>
     )
   }
 }
-
 
 export default App ;
